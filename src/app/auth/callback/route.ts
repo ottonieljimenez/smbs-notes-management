745cc222f2eeeb2a55cb2f3457f6174a3310bec1
Ottoniel Jimenez <ottoniel.herrera@gmail.com>
2024-03-27 11:13:27 -0600

    Update route.ts to set the session token in a cookie

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,6 +1,8 @@
+import Cookies from 'js-cookie'
+import { NextRequest, NextResponse } from 'next/server'
+
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import { NextRequest, NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
 
@@ -10,9 +12,24 @@ export async function GET (request: NextRequest) {
 
     if (code != null) {
         const supabase = createRouteHandlerClient({ cookies }) 
-        // using the code that we have passed via the URL
-        // we get back the user session
-        await supabase.auth.exchangeCodeForSession(code)
+        // Exchange the code for a session
+        const response = await supabase.auth.exchangeCodeForSession(code)
+
+        // Check if there's an error in the response
+        if (response.error) {
+            console.error('Error exchanging code for session:', response.error.message)
+            // Return a simple error response with the status code
+            return new NextResponse(response.error.message, { status: 500 })
+        }
+
+        // Extract session from the response
+        const session = response.data.session
+
+        // Set the session token in a cookie
+        Cookies.set('session', JSON.stringify(session))
+
+        // Redirect the user to a different page
+        //return NextResponse.redirect('/dashboard')
     }
 
     return NextResponse.redirect(requestUrl.origin)
